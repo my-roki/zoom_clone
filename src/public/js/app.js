652321f94d1d01bfe4db717eeb5d33b37acf5999
my-roki <anthonychangrok@gmail.com>
2022-04-12 18:39:34 +0900

    understanding web socket part

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert("hello js!");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("Just got this:", message.data, " -from server");
+});
+
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 3000);
+
+socket.addEventListener("close", () => {
+  console.log("Connected Server ❌");
+});
--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
+import http from "http";
+import { WebSocketServer } from "ws";
+
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,5 +16,29 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
+/* 
+// http
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListen);
+*/
+
+// Web Socket in Node JS
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server }); // after "ws": "^8.2.3" -> Websocket.Server (x) / WebSocketServer (o)
+
+wss.on("connection", (socket) => {
+  console.log("Connected Browser ✅");
+
+  socket.on("close", () => {
+    console.log("Disonnected from Browser ❌");
+  });
+
+  socket.on("message", (message) => {
+    console.log(message.toString("utf8"));
+  });
+
+  socket.send("Hello!");
+});
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+server.listen(3000, handleListen);
