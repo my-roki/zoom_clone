e9d334678726aa8bda5921c73bb27677a1cbb9ab
my-roki <anthonychangrok@gmail.com>
2022-04-12 15:23:53 +0900

    Zoom Clone initial commit - develope enviroment setting

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
